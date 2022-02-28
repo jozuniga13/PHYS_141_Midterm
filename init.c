4600a5f371df15ee23a4dcf18efdcc60ec17fd78
Yuntong Zhou <yuz102@its-may4623-09.ucsd.edu>
2022-02-28 14:39:56 -0800

    new file:   init.c 	new file:   meetingnotes.txt

---
+++ init.c
@@ -0,0 +1,12 @@
+/* This the initial setup for the two galaxies in mice project */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXPNT 3000            /* maximum number of points */
+#define PI 3.141592654
+
+
