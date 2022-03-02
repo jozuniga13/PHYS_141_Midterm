c14a35f5d319ed13ab6c39076a45070ded97bbb4
Yuntong Zhou <yuntongzhou@Yuntongs-MacBook-Air.local>
2022-03-01 20:40:17 -0800

    modified:   init.c 	modified:   meetingnotes.txt

--- init.c
+++ init.c
@@ -9,7 +9,21 @@
 #define MAXPNT 3000            /* maximum number of points */
 #define PI 3.141592654
 
-double M,m,Rmin,Rinit;
+double M,Rmin,Rinit;
+double G=1; 
 
+int main(argc, argv)
+int argc;
+char *argv[];
+{
+    M=pow(10,11); /* in solar mass */
+    Rmin=25*pow(10,3); /* in parsec */
+    /* to keep G as 1, convert M into a new mass unit */
+    M=M/
+
+
+
+    return 0;
+}
 
 
